8d66562d1d7efb84f51b97fde02a6084159029d7
VortexSerf64 <jinavossxjczbt@gmail.com>
2026-09-07 15:06:01 +0200

    Create FPS.cs

---
+++ FPS.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+    private float delta;
+    private float checkTime;
+    private int fps, frames;
+    
+    
+    private void LateUpdate()
+    {
+        delta += Time.deltaTime;
+        frames++;
+        float now = Time.realtimeSinceStartup;
+        if (now - checkTime > .125f)
+        {
+            float average = delta / frames;
+            fps = Mathf.FloorToInt(1f / average);
+            delta = 0;
+            frames = 0;
+            checkTime = now;
+        }
+    }
+    
+    
+    private void OnEnable()
+    {
+        DebugUI.TR += OnDebugUI;
+    }
+    private void OnDisable()
+    {
+        DebugUI.TR -= OnDebugUI;
+    }
+    private void OnDebugUI(StringBuilder builder)
+    {
+        builder.AppendLine(fps.PrepString());
+    }
+}
